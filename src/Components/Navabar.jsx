4a52cf91bbaf06f3389bfb2d4295ab93f513f0eb
manish8code <8manishgupta8@gmail.com>
2025-10-21 13:43:01 +0530

    nav completed

--- src/Components/Navabar.jsx
+++ src/Components/Navabar.jsx
@@ -1,30 +1,61 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
 import heartsvg from "../assets/heart.svg";
 import MediaQuery from 'react-responsive';
+import navOpen from '../assets/navOpen.svg';
+import navClose from '../assets/navClose.svg';
 
 const Navabar = () => {
-  return (
-    <div className="w-full h-14 bg-red-200/30 flex items-center justify-around sticky top-0 z-50 shadow-md  backdrop-blur-md"  >
+  const [menuOpen, setMenuOpen] = useState(false);
+  const location = useLocation(); // Get current route
 
-      <div className="text-5xl  font-bold font-romantic mx-5 hover:transform hover:scale-110 hover:duration-200 hover:text-shadow-md">
-        <NavLink to='/'><h1 className='text-[#722737]'> Lovely</h1></NavLink>
+  // Close mobile menu on route change
+  useEffect(() => {
+    setMenuOpen(false);
+  }, [location]);
+
+  const menuItems = (
+    <ul className="flex md:flex-row flex-col justify-around md:space-x-16 md:space-y-0 space-y-10">
+      <li className='svg-pink-drop-shadow'>
+        <NavLink to='/love1'><img src={heartsvg} className="w-9 h-8 svg-pink-drop-shadow" /></NavLink>
+      </li>
+      <li className='svg-pink-drop-shadow'>
+        <NavLink to='/love2' className="flex">
+          {[...Array(2)].map((_, i) => <img key={i} src={heartsvg} className="w-9 h-8 " />)}
+        </NavLink>
+      </li>
+      <li className='svg-pink-drop-shadow'>
+        <NavLink to='/love3' className="flex">
+          {[...Array(3)].map((_, i) => <img key={i} src={heartsvg} className="w-9 h-8 " />)}
+        </NavLink>
+      </li>
+    </ul>
+  );
+
+  return (
+    <div className="w-full h-14 bg-red-200/30 flex items-center justify-around sticky top-0 z-50 shadow-md backdrop-blur-md px-5">
+      
+      <div className="text-5xl md:w-[40%] font-bold font-romantic hover:scale-110 hover:duration-200">
+        <NavLink to='/'><h1 className='text-[#722737]'>Lovely</h1></NavLink>
       </div>
 
-      <MediaQuery minWidth={768}> 
-      <ul className="w-[35%]  flex justify-around  mx-5">
+      {/* Mobile Menu */}
+      <MediaQuery maxWidth={768}>
+        <img
+          src={menuOpen ? navClose : navOpen}
+          alt="Toggle Navigation"
+          className="w-8 h-8 cursor-pointer"
+          onClick={() => setMenuOpen(!menuOpen)}
+        />
+        {menuOpen && <div className="absolute top-14 right-0 bg-pink-400/30 shadow-md p-3 rounded">{menuItems}</div>}
+      </MediaQuery>
 
-      <NavLink to='/love1' className="ml-10 hover:transform hover:scale-110 hover:duration-200">
-      <li className='w-9 h-8 svg-pink-drop-shadow'><img src={heartsvg} /></li></NavLink>
-      <NavLink to='/love2' className="ml-10 hover:transform hover:scale-110 hover:duration-200">
-      <li className='w-13 h-8 flex svg-pink-drop-shadow'>{[...Array(2)].map((_, i) => <img key={i} src={heartsvg}  />)}</li></NavLink>
-      <NavLink to='/love3' className="ml-10 hover:transform hover:scale-110 hover:duration-200 ">
-      <li className='w-19 h-8 flex svg-pink-drop-shadow'>{[...Array(3)].map((_, i) => <img key={i} src={heartsvg}  />)}</li></NavLink>
-
-      </ul>
+      {/* Desktop Menu */}
+      <MediaQuery minWidth={769}>
+        {menuItems}
       </MediaQuery>
     </div>
-  )
-}
+  );
+};
 
-export default Navabar
+export default Navabar;
