e05c61261b2233d97a36e5d205d223ce9194a923
manish8code <8manishgupta8@gmail.com>
2025-10-21 12:45:01 +0530

    checkin

--- src/Pages/Love3.jsx
+++ src/Pages/Love3.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Love3 = () => {
   return (
     <div className='h-dvh bg-blue-300 flex items-center justify-center text-4xl font-bold'>
-      love 3 there is some changes 
+      love 
     </div>
   )
 }
