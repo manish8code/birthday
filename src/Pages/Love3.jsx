e469ada71c1f6232c4625093bd5cd5cbe17ac5ba
manish8code <8manishgupta8@gmail.com>
2025-10-29 02:20:47 +0530

    only font load at start

--- src/Pages/Love3.jsx
+++ src/Pages/Love3.jsx
@@ -1,13 +1,26 @@
-import React, { useState } from 'react'
-import Propose from '../Components/Propose'
-import AfterPropose from '../Components/AfterPropose';
-const Love3 = () => {
-  const [yes,setYes]=useState(false);
-  return (
-    <div className='' >
-           {(!yes) ? <Propose  setYes={setYes} /> : <AfterPropose />}
-    </div>
-  )
-}
+import React, { useState, useEffect } from "react";
+import Propose from "../Components/Propose";
+import AfterPropose from "../Components/AfterPropose";
+import kiss1 from "../assets/kiss1.gif";
+import kiss2 from "../assets/kiss2.gif";
 
-export default Love3
+const Love3 = () => {
+  const [yes, setYes] = useState(false);
+
+  // ✅ Preload images when component mounts
+  useEffect(() => {
+    const images = [kiss1, kiss2];
+    images.forEach((src) => {
+      const img = new Image();
+      img.src = src;
+    });
+  }, []);
+
+  return (
+    <div className="">
+      {!yes ? <Propose setYes={setYes} /> : <AfterPropose />}
+    </div>
+  );
+};
+
+export default Love3;
