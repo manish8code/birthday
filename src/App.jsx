a81e9e3abd73f0ac4973732634ed80029134b699
manish8code <8manishgupta8@gmail.com>
2025-10-29 02:53:46 +0530

    made the bg to load at start

--- src/App.jsx
+++ src/App.jsx
@@ -148,7 +148,7 @@ const App = () => {
 
         // 🧠 STEP 3: Preload all app images
         const imageModules = {
-          load1,load2,load3,bg
+          load1,load2,load3,bg,
           //...import.meta.glob("./assets/**/*.{jpg,jpeg,png,svg,gif}", { eager: true }),
          // ...import.meta.glob("./Images/**/*.{jpg,jpeg,png,svg,gif}", { eager: true }),
         };
