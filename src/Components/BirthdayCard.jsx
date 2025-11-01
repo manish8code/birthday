a158c4d74fb3c328d3e5d1f11c4741509afaa716
manish8code <8manishgupta8@gmail.com>
2025-11-01 23:46:30 +0530

    button text changed

--- src/Components/BirthdayCard.jsx
+++ src/Components/BirthdayCard.jsx
@@ -2,7 +2,7 @@ import "./birthdayCard.css";
 export default () => (
   <div className="birthdayCard h-50 lg:w-72 w-64 ">
     <div className="cardFront">
-      <h3 className="happy">HAPPY BIRTHDAY Love!</h3>
+      <h3 className="happy">HAPPY BIRTHDAY my Love!</h3>
       <div className="balloons">
         <div className="balloonOne" />
         <div className="balloonTwo" />
@@ -15,9 +15,10 @@ export default () => (
       <h3 className="back">Best Wishes from me!</h3>
       <p>Dear Pooja,</p>
       <p>
-        Happy birthday!! I hope your day is filled with lots of love and
+        A very Happiest birthday !! ... I hope your day is filled with lots of love and
         laughter! May all of your birthday wishes come true.
       </p>
+      <p className="lovely">Your love</p>
       <p className="name">Manish</p>
     </div>
   </div>
